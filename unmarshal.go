eefb37fefa5092501f39f0f41015299ef2c42e9c
Abdul Dakkak <abduld@wolfram.com>
2017-11-12 10:14:12 -0700

    work on dllayer

--- unmarshal.go
+++ unmarshal.go
@@ -20,9 +20,28 @@ func unmarshal(target proto.Message, protoFileName string) error {
 	return proto.Unmarshal(buf, target)
 }
 
+func unmarshalText(target proto.Message, protoFileName string) error {
+	if !com.IsFile(protoFileName) {
+		return errors.Errorf("%s is not a file", protoFileName)
+	}
+	buf, err := ioutil.ReadFile(protoFileName)
+	if err != nil {
+		return errors.Wrapf(err, "failed to open %s", protoFileName)
+	}
+
+	return proto.UnmarshalText(string(buf), target)
+}
+
 // ReadBlob ...
 func ReadBlob(protoFileName string) (*BlobProto, error) {
 	blob := new(BlobProto)
 	err := unmarshal(blob, protoFileName)
 	return blob, err
+}
+
+// ReadBlob ...
+func ReadNet(protoTextFileName string) (*NetParameter, error) {
+	net := new(NetParameter)
+	err := unmarshalText(net, protoTextFileName)
+	return net, err
 }
