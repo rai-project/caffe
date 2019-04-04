52bcc2b73503c2216a50f2b0453475b07d9699eb
Abdul Dakkak <abduld@wolfram.com>
2019-04-04 18:51:19 -0500

    update name

--- predict/predict.go
+++ predict/predict.go
@@ -16,7 +16,7 @@ import (
 	"github.com/rai-project/dlframework"
 	"github.com/rai-project/dlframework/framework/agent"
 	"github.com/rai-project/dlframework/framework/options"
-	common "github.com/rai-project/dlframework/framework/predict"
+	common "github.com/rai-project/dlframework/framework/predictor"
 	"github.com/rai-project/downloadmanager"
 	gocaffe "github.com/rai-project/go-caffe"
 	"github.com/rai-project/image"
