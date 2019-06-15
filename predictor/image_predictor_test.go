4ba09d8c3c618df1f9ff0ee5df2678ec804a4951
Abdul Dakkak <abduld@wolfram.com>
2019-06-15 13:51:58 -0500

    fixes predictor test

--- predictor/image_predictor_test.go
+++ predictor/image_predictor_test.go
@@ -2,67 +2,34 @@ package predictor
 
 import (
 	"context"
-	"image"
-	"os"
-	"path/filepath"
 	"testing"
 
 	"github.com/k0kubun/pp"
 	"github.com/rai-project/caffe"
 	"github.com/rai-project/dlframework/framework/options"
 	raiimage "github.com/rai-project/image"
+	imageexamples "github.com/rai-project/image/examples"
 	"github.com/rai-project/image/types"
 	nvidiasmi "github.com/rai-project/nvidia-smi"
 	"github.com/stretchr/testify/assert"
 	gotensor "gorgonia.org/tensor"
 )
 
-func normalizeImageHWC(in0 image.Image, mean []float32, scale float32) ([]float32, error) {
-	height := in0.Bounds().Dy()
-	width := in0.Bounds().Dx()
-	out := make([]float32, 3*height*width)
-	switch in := in0.(type) {
-	case *types.RGBImage:
-		for y := 0; y < height; y++ {
-			for x := 0; x < width; x++ {
-				offset := y*in.Stride + x*3
-				rgb := in.Pix[offset : offset+3]
-				r, g, b := rgb[0], rgb[1], rgb[2]
-				out[offset+0] = (float32(r) - mean[0]) / scale
-				out[offset+1] = (float32(g) - mean[1]) / scale
-				out[offset+2] = (float32(b) - mean[2]) / scale
-			}
-		}
-	case *types.BGRImage:
-		for y := 0; y < height; y++ {
-			for x := 0; x < width; x++ {
-				offset := y*in.Stride + x*3
-				bgr := in.Pix[offset : offset+3]
-				b, g, r := bgr[0], bgr[1], bgr[2]
-				out[offset+0] = (float32(b) - mean[0]) / scale
-				out[offset+1] = (float32(g) - mean[1]) / scale
-				out[offset+2] = (float32(r) - mean[2]) / scale
-			}
-		}
-	default:
-		panic("unreachable")
-	}
-
-	return out, nil
-}
-
-func normalizeImageCHW(in *types.RGBImage, mean []float32, scale float32) ([]float32, error) {
+func normalizeImageCHW(in types.Image, mean []float32, scale float32) ([]float32, error) {
 	height := in.Bounds().Dy()
 	width := in.Bounds().Dx()
+	channels := in.Channels()
+	stride := width * channels
+	pix := in.Pixels()
 	out := make([]float32, 3*height*width)
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
-			offset := y*in.Stride + x*3
-			rgb := in.Pix[offset : offset+3]
+			offset := y*stride + x*channels
+			rgb := pix[offset : offset+channels]
 			r, g, b := rgb[0], rgb[1], rgb[2]
-			out[y*width+x] = (float32(b) - mean[0]) / scale
+			out[y*width+x] = (float32(r) - mean[0]) / scale
 			out[width*height+y*width+x] = (float32(g) - mean[1]) / scale
-			out[2*width*height+y*width+x] = (float32(r) - mean[2]) / scale
+			out[2*width*height+y*width+x] = (float32(b) - mean[2]) / scale
 		}
 	}
 	return out, nil
@@ -107,13 +74,6 @@ func TestImageClassification(t *testing.T) {
 	assert.NotEmpty(t, predictor)
 	defer predictor.Close()
 
-	imgDir, _ := filepath.Abs("./_fixtures")
-	imgPath := filepath.Join(imgDir, "penguin.jpg")
-	r, err := os.Open(imgPath)
-	if err != nil {
-		panic(err)
-	}
-
 	preprocessOpts, err := predictor.GetPreprocessOptions()
 	assert.NoError(t, err)
 	channels := preprocessOpts.Dims[0]
@@ -121,31 +81,28 @@ func TestImageClassification(t *testing.T) {
 	width := preprocessOpts.Dims[2]
 	mode := preprocessOpts.ColorMode
 
-	var imgOpts []raiimage.Option
-	if mode == types.RGBMode {
-		imgOpts = append(imgOpts, raiimage.Mode(types.RGBMode))
-	} else {
-		imgOpts = append(imgOpts, raiimage.Mode(types.BGRMode))
+	imgOpts := []raiimage.Option{
+		raiimage.Mode(mode),
+		raiimage.Width(width),
+		raiimage.Height(height),
+		raiimage.ResizeAlgorithm(types.ResizeAlgorithmLinear),
 	}
 
-	img, err := raiimage.Read(r, imgOpts...)
+	img, err := imageexamples.Get("platypus", imgOpts...)
 	if err != nil {
 		panic(err)
 	}
 
-	imgOpts = append(imgOpts, raiimage.Resized(height, width))
-	imgOpts = append(imgOpts, raiimage.ResizeAlgorithm(types.ResizeAlgorithmLinear))
-	resized, err := raiimage.Resize(img, imgOpts...)
+	imgFloats, err := normalizeImageCHW(img, preprocessOpts.MeanImage, preprocessOpts.Scale)
+	if err != nil {
+		panic(err)
+	}
 
 	input := make([]*gotensor.Dense, batchSize)
-	imgFloats, err := normalizeImageHWC(resized, preprocessOpts.MeanImage, preprocessOpts.Scale)
-	if err != nil {
-		panic(err)
-	}
-
 	for ii := 0; ii < batchSize; ii++ {
-		input[ii] = gotensor.New(
-			gotensor.WithShape(height, width, channels),
+		input[ii] = gotensor.NewDense(
+			gotensor.Float32,
+			gotensor.Shape([]int{height, width, channels}),
 			gotensor.WithBacking(imgFloats),
 		)
 	}
